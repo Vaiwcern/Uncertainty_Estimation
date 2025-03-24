12e578d1a18043b80de52c7a06bd6f857e49a6e6
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2025-03-24 17:52:22 +0700

    x

--- train/iterative_train.py
+++ train/iterative_train.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     # Set CUDA_VISIBLE_DEVICES để chọn GPU
     os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
 
-    BATCH_SIZE = 21
+    BATCH_SIZE = 18
     LR = 1e-3
     EPOCHS = 10
 
