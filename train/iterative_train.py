097c9c9b96e3b5ef39969d5549f00afc220cf695
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2025-03-25 19:06:36 +0700

    x

--- train/iterative_train.py
+++ train/iterative_train.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
         model.build((None, 1024, 1024, 4))
         model.load_weights("model_epoch_10.weights.h5")
 
-    predict_and_save(model, dataset=test_dataset, image_files, save_dir=os.path.join(trainparam.save_path, "predict"))
+    predict_and_save(model, dataset=test_dataset, image_files=image_files, save_dir=os.path.join(trainparam.save_path, "predict"))
