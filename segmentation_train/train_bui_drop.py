442fc347d7f57acfc2e53fbb9fc0fbcfe3bc505f
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2025-04-10 06:02:49 +0700

    x

--- segmentation_train/train_bui_drop.py
+++ segmentation_train/train_bui_drop.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from types import SimpleNamespace
 import keras
 from CustomDataset.MyDS import MyDSTF
-from model.unet import StandardUNet
+from model.unet import StandardUNet, VanilaUnet
 from utils import focal_loss, IoUMetric, F1ScoreMetric
 import tensorflow as tf
 import numpy as np
@@ -30,8 +30,14 @@ def parse_args():
 class PrintLossCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
         loss = logs.get("loss")
+        acc = logs.get("accuracy")
+        log_str = f"\n✅ Epoch {epoch + 1}: "
         if loss is not None:
-            print(f"\n✅ Epoch {epoch + 1}: Average Loss = {loss:.4f}")
+            log_str += f"Loss = {loss:.4f} | "
+        if acc is not None:
+            log_str += f"Acc = {acc:.4f} | "
+
+        print(log_str.rstrip(" |"))
 
 
 class SaveEveryNEpoch(tf.keras.callbacks.Callback):
@@ -50,7 +56,7 @@ class SaveEveryNEpoch(tf.keras.callbacks.Callback):
             print(f"\n📦 Saved model to: {filename}")
 
 
-def convert_to_functional(model, input_shape=(256, 256, 3)):
+def convert_to_functional(model, input_shape=(1024, 1024, 4)):
     inputs = keras.Input(shape=input_shape)
     outputs = model(inputs)
     return keras.Model(inputs=inputs, outputs=outputs)
@@ -66,29 +72,12 @@ def predict_and_save(model, dataset, image_files, save_dir):
         if idx >= total:
             break  # Ngăn vòng lặp chạy mãi do repeat()
 
-        x_orig = x[..., :3].numpy()  # Bỏ channel thứ 4, về numpy
-        zero_channel = tf.zeros_like(x[..., :1])
+        batch_size = x.shape[0]
 
         outputs = []
-        batch_size = x.shape[0]
-        gradcams_by_batch = [[] for _ in range(batch_size)]
-
-        for i in range(3):
-            x_4ch = tf.concat([x[..., :3], zero_channel], axis=-1)
-            y_pred = model(x_4ch, training=False)
-            outputs.append(y_pred)
-            zero_channel = y_pred
-
-            for j in range(batch_size):
-                prop_from_layer = model.layers[-1].name
-                prop_to_layer = 'center_block'
-                cls = 0
-
-                clsroi = ClassRoI(model=model, image=x_4ch[j], cls=cls)
-                newsgc = SegGradCAM(model, x_4ch[j], cls, prop_to_layer, prop_from_layer, roi=clsroi,
-                                    normalize=True, abs_w=False, posit_w=False)
-                mymap = newsgc.SGC()  # Heatmap với shape (H, W)
-                gradcams_by_batch[j].append(mymap)
+        for k in range(5):
+            output = model(x, training=True)
+            outputs.append(output)
 
         for b in range(batch_size):
             if idx >= len(image_files):
@@ -98,27 +87,16 @@ def predict_and_save(model, dataset, image_files, save_dir):
             name_without_ext = os.path.splitext(image_name)[0]
 
             # Lưu ảnh gốc (convert về [0,255])
-            ori_image = (x_orig[b] * 255).astype("uint8")
+            ori_image = (x[b] * 255).numpy().astype("uint8")
             ori_path = os.path.join(save_dir, f"{name_without_ext}_input.png")
             imageio.imwrite(ori_path, ori_image)
 
-            # Lưu từng output mà không threshold
-            for i in range(3):
-                pred = outputs[i][b]  # (H, W, 1)
-                pred = tf.squeeze(pred, axis=-1)
-                pred = (pred * 255).numpy().astype("uint8")  # scale lên [0, 255] mà không threshold
-                output_path = os.path.join(save_dir, f"{name_without_ext}_output_{i}.png")
-                imageio.imwrite(output_path, pred)
-
-                grad = gradcams_by_batch[b][i]
-                if isinstance(grad, tf.Tensor):
-                    grad = grad.numpy()
-                grad = (grad * 255).astype("uint8")
-                grad_path = os.path.join(save_dir, f"{name_without_ext}_grad_{i}.png")
-                imageio.imwrite(grad_path, grad)
+            for k in range(5):
+                output_image = (outputs[k][b] * 255).numpy().astype("uint8")
+                output_path = os.path.join(save_dir, f"{name_without_ext}_output_{k}.png")
+                cv2.imwrite(output_path, output_image)
 
             idx += 1
-
 
 if __name__ == "__main__":
     # Lấy đối số GPU từ dòng lệnh
@@ -133,12 +111,12 @@ if __name__ == "__main__":
 
     # Ensure steps_per_epoch and validation_steps are integers
     trainparam = SimpleNamespace(
-        dataset_name="RoadTracer",
+        dataset_name="BUI",
         learning_rate=LR,
         n_classes=1,
         epochs=EPOCHS,
         batch_size=BATCH_SIZE,
-        input_shape=(256, 256, 4),
+        input_shape=(256, 256, 3),
         save_path="/home/ltnghia02/MEDICAL_ITERATIVE/model/BUI256_DROP"
     )
 
@@ -151,6 +129,7 @@ if __name__ == "__main__":
 
     train_dataset_wrapper = MyDSTF(
         dataset_dir="/home/ltnghia02/MEDICAL_ITERATIVE/Dataset/BUI_256",
+        channel=3,
         batch_size=trainparam.batch_size,
         normalize=True,
         train=True,
@@ -167,7 +146,7 @@ if __name__ == "__main__":
     print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
     with strategy.scope():
-        model = StandardUNet(input_channels=4, dropout_rate=0.1)
+        model = VanilaUnet(input_channels=3, dropout_rate=0.1)
         optim = keras.optimizers.Adam(learning_rate=trainparam.learning_rate)
         model.compile(
             optimizer=optim,
@@ -179,10 +158,8 @@ if __name__ == "__main__":
             ]
         )
 
-        model.build(input_shape=(None, 256, 256, 4))
+        model.build(input_shape=(None, 256, 256, 3))
 
-        # dummy_x = tf.random.normal((1, 1024, 1024, 4))
-        # _ = model(dummy_x)
 
     model.fit(
         train_dataset,
@@ -217,9 +194,40 @@ if __name__ == "__main__":
 
     # strategy = tf.distribute.MirroredStrategy()
     # with strategy.scope():
-    #     model = StandardUNet(input_channels=4, dropout_rate=0.0)
-    #     model.build((None, 1024, 1024, 4))
+    #     model = VanilaUnet(input_channels=3, dropout_rate=0.1)
+    #     model.build((None, 1024, 1024, 3))
     #     model.load_weights(os.path.join(trainparam.save_path, "model_epoch_55.weights.h5"))
-    #     model = convert_to_functional(model, input_shape=(1024, 1024, 4))
+    #     model = convert_to_functional(model, input_shape=(1024, 1024, 3))
 
-    # predict_and_save(model, dataset=test_dataset, image_files=image_files, save_dir=os.path.join(trainparam.save_path, "predict"))
+    # predict_and_save(model, dataset=test_dataset, image_files=image_files, save_dir=os.path.join(trainparam.save_path, "predict_epoch_55"))
+
+    # PREDICT massachusetts
+    # log_file_path = os.path.join(trainparam.save_path, "log_predict_mass.txt")
+    # os.makedirs(trainparam.save_path, exist_ok=True)  # Đảm bảo thư mục tồn tại
+    #
+    # sys.stdout = open(log_file_path, "w")
+    # sys.stderr = sys.stdout
+    #
+    # test_dataset_wrapper = MassachusettsDatasetTF(
+    #     dataset_dir="/home/ltnghia02/MEDICAL_ITERATIVE/Dataset/Massachusetts_Crop",
+    #     batch_size=BATCH_SIZE,
+    #     split='test',
+    #     channel=3,
+    #     normalize=True
+    # )
+    #
+    # test_dataset = test_dataset_wrapper.dataset
+    # image_files = [str(p) for p in test_dataset_wrapper.image_files]
+    #
+    # print("Total images:", len(test_dataset_wrapper.image_files))
+    # print("Steps per epoch:", test_dataset_wrapper.steps_per_epoch)
+    #
+    # strategy = tf.distribute.MirroredStrategy()
+    # with strategy.scope():
+    #     model = VanilaUnet(input_channels=3, dropout_rate=0.1)
+    #     model.build((None, 512, 512, 3))
+    #     model.load_weights(os.path.join(trainparam.save_path, "model_epoch_55.weights.h5"))
+    #     model = convert_to_functional(model, input_shape=(512, 512, 3))
+    #
+    # predict_and_save(model, dataset=test_dataset, image_files=image_files,
+    #                  save_dir=os.path.join(trainparam.save_path, "predict_epoch_55_mass"))
