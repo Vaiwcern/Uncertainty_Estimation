fed7ecf11b61b3dfb2b3d7e16b9431f72aa13e19
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2025-04-10 20:32:26 +0700

    x

--- segmentation_train/train_bui_iter.py
+++ segmentation_train/train_bui_iter.py
@@ -143,56 +143,56 @@ if __name__ == "__main__":
     )
 
     # TRAIN
-    log_file_path = os.path.join(trainparam.save_path, "log.txt")
-    os.makedirs(trainparam.save_path, exist_ok=True)  # Đảm bảo thư mục tồn tại
-
-    sys.stdout = open(log_file_path, "w")
-    sys.stderr = sys.stdout
-
-    train_dataset_wrapper = MyDSTF(
-        dataset_dir="/home/ltnghia02/MEDICAL_ITERATIVE/Dataset/BUI_256",
-        batch_size=trainparam.batch_size,
-        normalize=True,
-        train=True,
-        thin_label=False
-    )
-
-    train_dataset = train_dataset_wrapper.dataset
-
-    print("Total images:", len(train_dataset_wrapper.image_files))
-    print("Steps per epoch:", train_dataset_wrapper.steps_per_epoch)
-
-    # Create a MirroredStrategy.
-    strategy = tf.distribute.MirroredStrategy()
-    print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
-
-    with strategy.scope():
-        model = StandardUNet(input_channels=4, dropout_rate=0.0)
-        optim = keras.optimizers.Adam(learning_rate=trainparam.learning_rate)
-        model.compile(
-            optimizer=optim,
-            loss=focal_loss(),
-            metrics=[
-                'accuracy',
-                IoUMetric(),
-                F1ScoreMetric()
-            ]
-        )
-
-        model.build(input_shape=(None, 256, 256, 4))
-
-        dummy_x = tf.random.normal((1, 256, 256, 4))
-        _ = model(dummy_x)
-
-    model.fit(
-        train_dataset,
-        epochs=trainparam.epochs,
-        steps_per_epoch=train_dataset_wrapper.steps_per_epoch,
-        callbacks=[
-            PrintLossCallback(),
-            SaveEveryNEpoch(save_path=trainparam.save_path, interval=10)
-        ]
-    )
+    # log_file_path = os.path.join(trainparam.save_path, "log.txt")
+    # os.makedirs(trainparam.save_path, exist_ok=True)  # Đảm bảo thư mục tồn tại
+    #
+    # sys.stdout = open(log_file_path, "w")
+    # sys.stderr = sys.stdout
+    #
+    # train_dataset_wrapper = MyDSTF(
+    #     dataset_dir="/home/ltnghia02/MEDICAL_ITERATIVE/Dataset/BUI_256",
+    #     batch_size=trainparam.batch_size,
+    #     normalize=True,
+    #     train=True,
+    #     thin_label=False
+    # )
+    #
+    # train_dataset = train_dataset_wrapper.dataset
+    #
+    # print("Total images:", len(train_dataset_wrapper.image_files))
+    # print("Steps per epoch:", train_dataset_wrapper.steps_per_epoch)
+    #
+    # # Create a MirroredStrategy.
+    # strategy = tf.distribute.MirroredStrategy()
+    # print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+    #
+    # with strategy.scope():
+    #     model = StandardUNet(input_channels=4, dropout_rate=0.0)
+    #     optim = keras.optimizers.Adam(learning_rate=trainparam.learning_rate)
+    #     model.compile(
+    #         optimizer=optim,
+    #         loss=focal_loss(),
+    #         metrics=[
+    #             'accuracy',
+    #             IoUMetric(),
+    #             F1ScoreMetric()
+    #         ]
+    #     )
+    #
+    #     model.build(input_shape=(None, 256, 256, 4))
+    #
+    #     dummy_x = tf.random.normal((1, 256, 256, 4))
+    #     _ = model(dummy_x)
+    #
+    # model.fit(
+    #     train_dataset,
+    #     epochs=trainparam.epochs,
+    #     steps_per_epoch=train_dataset_wrapper.steps_per_epoch,
+    #     callbacks=[
+    #         PrintLossCallback(),
+    #         SaveEveryNEpoch(save_path=trainparam.save_path, interval=10)
+    #     ]
+    # )
 
     # PREDICT
     log_file_path = os.path.join(trainparam.save_path, "log_predict.txt")
@@ -218,9 +218,9 @@ if __name__ == "__main__":
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         model = StandardUNet(input_channels=4, dropout_rate=0.0)
-        model.build((None, 1024, 1024, 4))
+        model.build((None, 256, 256, 4))
         model.load_weights(os.path.join(trainparam.save_path, "model_epoch_100.weights.h5"))
-        model = convert_to_functional(model, input_shape=(1024, 1024, 4))
+        model = convert_to_functional(model, input_shape=(256, 256, 4))
 
     predict_and_save(model, dataset=test_dataset, image_files=image_files,
                      save_dir=os.path.join(trainparam.save_path, "predict_epoch_100"))
