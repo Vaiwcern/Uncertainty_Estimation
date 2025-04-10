eebb3505952678798de1cf2b1e07f8510f832aa6
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2025-04-10 18:12:43 +0700

    x

--- CustomDataset/MyDS.py
+++ CustomDataset/MyDS.py
@@ -30,7 +30,7 @@ class MyDSTF:
     def load_pair(self, image_path, mask_path):
         image = imageio.imread(image_path.decode("utf-8"))
         # mask = imageio.imread(mask_path.decode("utf-8"))[:, :, 0]
-        mask = imageio.imread(mask_path.decode("utf-8"), as_gray=True)
+        mask = imageio.imread(mask_path.decode("utf-8"), mode='L')
         mask = (mask >= 128).astype(np.float32)
         mask = np.expand_dims(mask, axis=-1)
 
